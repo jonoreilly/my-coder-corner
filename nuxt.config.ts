6e6f076b87ec0294e514a66fc3b5140d97843c32
Jon O'Reilly del Cerro <jon.oreilly@easybell.de>
2022-07-10 23:41:52 +0200

    feat: add SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@nuxt/content"],
+  nitro: {
+    prerender: {
+      routes: ["/sitemap.xml"],
+    },
+  },
 });
