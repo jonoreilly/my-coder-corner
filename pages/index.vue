c075988b862c235111850e11e1e428201d93c488
Jon O'Reilly del Cerro <jon.oreilly@easybell.de>
2022-07-10 19:29:03 +0200

    feat: use nuxt 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
-<template>
-  <Tutorial/>
-</template>
+<script setup lang="ts">
+const { data: joke } = await useFetch<any>("https://v2.jokeapi.dev/joke/Any");
 
-<script lang="ts">
-import Vue from 'vue'
+const counter = useState("counter", () => 0);
 
-export default Vue.extend({})
+function add() {
+  counter.value++;
+}
+
+function subtract() {
+  counter.value--;
+}
 </script>
+
+<template>
+  <div>
+    <h1>Index page</h1>
+
+    <div>Counter: {{ counter }}</div>
+
+    <div>
+      <button @click="add()">+</button>
+
+      <button @click="subtract()">-</button>
+    </div>
+
+    <div>
+      Category: {{ joke.category }}
+
+      <p v-if="joke.type === 'single'">{{ joke.joke }}</p>
+
+      <p v-else>{{ joke.setup }}<br />{{ joke.delivery }}</p>
+    </div>
+
+    <div>
+      {{ JSON.stringify(joke) }}
+    </div>
+  </div>
+</template>
